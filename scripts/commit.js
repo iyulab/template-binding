914e5cd73b30b6e54f27608e802eab93edd6892b
junehyung <jhcj910805@gmail.com>
2024-02-01 00:32:11 +0900

    update

--- scripts/commit.js
+++
@@ -1,7 +0,0 @@
-import fs from 'fs';
-import { execSync } from 'child_process';
-
-const packageJson = JSON.parse(fs.readFileSync(new URL('../package.json', import.meta.url), 'utf8'));
-const version = packageJson.version;
-
-execSync(`git add . && git commit -m "update: version ${version}" && git pull && git push`, { stdio: 'inherit' });
