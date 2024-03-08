5c688d7f77ba27549c312c3ede1e469ef925061c
junehyung <jhcj910805@gmail.com>
2024-03-08 18:44:45 +0900

    Bind 작업중

--- src/core/Bind.ts
+++ src/core/Bind.ts
@@ -1,39 +1,60 @@
-// src/core/Bind.ts
-import dayjs from "dayjs";
-import numeral from "numeral";
+import jsonata from "jsonata";
 
-// 바인딩 표현식 처리
-// 표현식의 예시
-// 기본형: ${name}
-// 기본값: ${name || tom}
-// 배열: ${items[0]}
-// 배열의 속성: ${items[4].name}
-// 포멧: ${date:yy-MM-dd}, day.js 사용
-// 포멧과 기본값: ${date:yy-MM-dd || 2020-01-01}
-// 숫자포멧: ${number:0,0.00}
-// 숫자포멧과 기본값: ${number:0,0.00 || 0}
-// 배열과 포멧: ${items[0]:yy-MM-dd}
-// 루트요소가 배열일 경우 첫번째 원소: ${[0]}
+type DataProvider = ( APIProvider ) & { 
+  /**
+   * 데이터 제공자의 업데이트 주기를 설정합니다.
+   * 단위는 밀리초(ms)이며, 0보다 큰 값이어야 합니다.
+   */
+  interval?: number 
+};
 
-class Bind {
-  // ## private methods...
-  private setProps(target: any, props: any) {
+interface APIProvider {
+  type: 'webapi';
+  url: string;
+  method?: 'GET' | 'POST' | 'PUT' | 'DELETE' | 'HEAD' | 'PATCH';
+  headers?: HeadersInit;
+  body?: BodyInit;
+}
+
+// TODO: 현재 옵션의 역할은 타이머를 관리하는 것입니다.
+interface BindOptions {
+  refreshId?: NodeJS.Timeout;
+}
+
+/**
+ * The `Bind` class provides methods for binding properties and data to a target object.
+ * It supports binding expressions using the jsonata syntax and allows for data updates from a data provider.
+ * @see https://template-binding.github.io
+ */
+export class Bind {
+
+  /** ==================== Bind Field ==================== **/
+ 
+  
+
+  /** ==================== Bind Field ==================== **/
+  
+
+
+  /** ==================== Get Set Methods Start ==================== **/
+
+  private static setProps(target: any, props: any) {
     target.__$bind_props__ = props;
   }
 
-  private setData(target: any, data: any) {
+  private static setData(target: any, data: any) {
     target.__$bind_data__ = data;
   }
 
-  private setDataProvider(target: any, dataProvider: any) {
+  private static setDataProvider(target: any, dataProvider: any) {
     target.__$bind_data_provider__ = dataProvider;
   }
 
-  private setOptions(target: any, options: any) {
+  private static setOptions(target: any, options: BindOptions) {
     target.__$bind_data_options__ = options;
   }
 
-  private getProps(target: any) {
+  private static getProps(target: any) {
     const props = target.__$bind_props__;
     if (props == null) {
       this.setProps(target, {});
@@ -41,7 +62,7 @@ class Bind {
     return target.__$bind_props__;
   }
 
-  private getData(target: any) {
+  private static getData(target: any) {
     const data = target.__$bind_data__;
     if (data == null) {
       this.setData(target, {});
@@ -49,7 +70,7 @@ class Bind {
     return target.__$bind_data__;
   }
 
-  private getDataProvider(target: any) {
+  private static getDataProvider(target: any): DataProvider {
     const dataProvider = target.__$bind_data_provider__;
     if (dataProvider == null) {
       this.setDataProvider(target, {});
@@ -57,7 +78,7 @@ class Bind {
     return target.__$bind_data_provider__;
   }
 
-  private getOptions(target: any) {
+  private static getOptions(target: any): BindOptions {
     const options = target.__$bind_data_options__;
     if (options == null) {
       this.setOptions(target, {});
@@ -65,143 +86,83 @@ class Bind {
     return target.__$bind_data_options__;
   }
 
-  private applyFormat(value: any, format: string) {
-    // 숫자 또는 숫자 형태의 문자열인 경우
-    if (
-      typeof value === "number" ||
-      (typeof value === "string" &&
-        /^[+-]?(\d+|\d+\.\d*|\.\d+)([eE][+-]?\d+)?$/.test(value))
-    ) {
-      return numeral(value).format(format);
-    }
-    // value가 Date 객체인 경우
-    else if (value instanceof Date) {
-      return dayjs(value).format(format);
-    }
-    // value가 문자열이고, ISO 날짜 형식에 맞는 경우
-    else if (typeof value === "string" && !isNaN(Date.parse(value))) {
-      return dayjs(value).format(format);
-    } else {
-      // 알 수 없는 타입에 대한 처리
-      console.error("Unknown value type for formatting", value);
-      return value;
-    }
-  }
+  /** ==================== Get Set Methods End ==================== **/
 
-  private resolveValue(
-    data: any,
-    key: string,
-    defaultValue: any = "",
-    format: string | undefined = undefined
-  ): any {
-    let value = data;
-    const pathParts = key.split(".");
 
-    // 첫 번째 요소가 배열 인덱스일 경우
-    if (pathParts.length > 0 && pathParts[0].match(/^\[\d+\]$/)) {
-      const indexMatch = pathParts[0].match(/^\[(\d+)\]$/);
-      if (indexMatch) {
-        const index = parseInt(indexMatch[1], 10);
-        value = Array.isArray(data) ? data[index] : undefined;
-        pathParts.shift(); // 이 부분을 처리한 후에는 배열에서 제거
-      }
-    }
 
-    for (const part of pathParts) {
-      if (value === undefined || value === null) {
-        return defaultValue;
-      }
+  /** ==================== Bind Main Methods Start ==================== **/
 
-      const arrayMatch = part.match(/^([^\[]+)(?:\[(\d+)\])?$/);
-      if (arrayMatch) {
-        const [, propName, index] = arrayMatch;
-        value = value[propName];
-
-        if (value && index !== undefined) {
-          value = value[parseInt(index, 10)];
+  // target에 props를 재귀적으로 바인딩합니다.
+  private static applyProps(target: any, props: any, data: any) {
+    Object.keys(props).forEach(async (propName) => {
+      // 1. 속성 값 가져오기
+      const value = props[propName];
+      if (typeof value === "string" && value.includes("{{")) {
+        // 2. 바인딩 표현식 처리
+        target[propName] = await this.findValue(props, propName, data);
+      } else if (typeof value === "object") {
+        // 2. 오브젝트 객체 처리
+        if (value === null) { // null인 경우
+          target[propName] = null;
+        } else if (Array.isArray(value)) { // 배열인 경우 => 재귀 호출
+          if (target[propName] === undefined) target[propName] = [];
+          target[propName] = [...target[propName]];
+          this.applyProps(target[propName], value, data);
+        } else { // 객체인 경우 => 재귀 호출
+          if (target[propName] === undefined) target[propName] = {};
+          target[propName] = { ...target[propName] };
+          this.applyProps(target[propName], value, data);
         }
       } else {
-        value = value[part];
+        // 2. 이외 기본 값 할당 => number, boolean, string(not expression), undefined
+        target[propName] = value;
       }
+    })
+  }
 
-      if (value === undefined) {
-        return defaultValue;
+  // FixMe: {{  }} 구분자가 섞여 있을경우 처리가 안됨 
+  // ex) "{{name}} {{age}}" 이경우 singleExpr로 처리됨 정규식 변경 필요
+  // props에 data를 바인딩합니다.(구분자: "{{ }}") (jsonata 문법)
+  private static async findValue(props: any, name: any, data: any) {
+    let value = props[name];
+    const singleExpr = /^\{\{(.+?)\}\}$/; // 단일 표현식 ex) "{{object}}" -> { name: "world" }
+    const multiExpr = /\{\{(.+?)\}\}/g; // 다중 표현식 ex) "Hello, {{object.name}}!" -> "Hello, world!"
+    if (singleExpr.test(value)) {
+      // 단일 표현식 처리 => 해당 타입으로 할당
+      const expr = singleExpr.exec(value);
+      if (expr && expr.length > 1) {
+        const expression = jsonata(expr[1]);
+        value = await expression.evaluate(data);
       }
+    } else if(multiExpr.test(value)) {
+      // 다중 표현식 처리 => String 타입으로 할당
+      return await this.replaceAsync(value, multiExpr, async (_, expr) => {
+        const expression = jsonata(expr);
+        return await expression.evaluate(data);
+      });
     }
-
-    if (format && value !== undefined) {
-      value = this.applyFormat(value, format);
-    }
-
+    // 표현식이 아니 었을 경우 => 그대로 할당
     return value;
   }
 
-  private parseExpression(expr: string) {
-    const [path, format] = expr.split(":").map((s) => s.trim());
-    const [key, defaultValue] = path.split("||").map((s) => s.trim());
-    return { key, defaultValue, format };
-  }
-
-  private applyProps(target: any, props: any, data: any) {
-    Object.keys(props).forEach((propName) => {
-      const propValue = props[propName];
-      if (propValue === undefined) {
-        target[propName] = undefined;
-      } else if (typeof propValue === "string" && propValue.includes("${")) {
-        // 바인딩 표현식 처리
-        const singleExpr = /^\$\{([^}]+)\}$/;
-        const multiExpr = /\$\{([^}]+)\}/g;
-        if (singleExpr.test(propValue)) {
-          const expr = singleExpr.exec(propValue);
-          if (!expr) return;
-          const { key, defaultValue, format } = this.parseExpression(expr[1]);
-          target[propName] = this.resolveValue(data, key, defaultValue, format);
-        } else {
-          target[propName] = propValue.replace(multiExpr, (_, expr) => {
-            const { key, defaultValue, format } = this.parseExpression(expr);
-            return this.resolveValue(data, key, defaultValue, format);
-          });
-        }
-      } else if (typeof propValue === "object") {
-        // 깊은 객체 처리
-        if (propValue === null) {
-          target[propName] = null;
-        } else if (Array.isArray(propValue)) {
-          if (target[propName] === undefined) target[propName] = [];
-          target[propName] = [...target[propName]];
-          this.applyProps(target[propName], propValue, data);
-        } else {
-          if (target[propName] === undefined) target[propName] = {};
-          target[propName] = { ...target[propName] };
-          this.applyProps(target[propName], propValue, data);
-        }
-      } else {
-        // 기본 값 할당
-        target[propName] = propValue;
-      }
+  private static async replaceAsync(str, regex, asyncFn) {
+    const promises = [];
+    str.replace(regex, (match, ...args) => {
+      const promise = asyncFn(match, ...args);
+      promises.push(promise);
     });
+    const data = await Promise.all(promises);
+    return str.replace(regex, () => data.shift());
   }
 
-  private async getDataByWebApiAsync(dataProvider: any) {
-    const url = dataProvider.url;
-    const method = dataProvider.method || "GET";
-    const headers = dataProvider.headers || {};
-    const fetchOptions = {
-      method,
-      headers,
-    } as any;
+  /** ==================== Bind Main Methods End ==================== **/
 
-    // POST 또는 다른 메서드인 경우에만 body를 추가
-    if (method !== "GET" && method !== "HEAD") {
-      const body = dataProvider.body || {};
-      fetchOptions["body"] = JSON.stringify(body);
-    }
 
-    const res = await fetch(url, fetchOptions);
-    return await res.json();
-  }
 
-  private async getDataByProviderAsync(dataProvider: any) {
+  /** ==================== DataProvider Main Methods End ==================== **/
+
+  // DataProvider의 타입에 따라 데이터를 가져옵니다.
+  private static async getDataByProviderAsync(dataProvider: DataProvider) {
     if (dataProvider.type === "webapi") {
       return await this.getDataByWebApiAsync(dataProvider);
     } else {
@@ -210,81 +171,142 @@ class Bind {
     }
   }
 
-  private async refreshData(target: any) {
+  // WebAPI로부터 데이터를 가져옵니다.
+  private static async getDataByWebApiAsync(dataProvider: DataProvider) {
+    const url = dataProvider.url;
+    const method = dataProvider.method || "GET";
+    const headers = dataProvider.headers || {};
+
+    // POST 또는 다른 메서드인 경우에만 body를 추가
+    const body = method !== "GET" && method !== "HEAD"
+    ? JSON.stringify(dataProvider.body || {})
+    : undefined;
+
+    const res = await fetch(url, {
+      method: method,
+      headers: headers,
+      body: body,
+    });
+    return await res.json();
+  }
+
+  // DataProvider 설정에 따라 데이터를 주기적으로 업데이트합니다.
+  private static async refreshData(target: any) {
     const dataProvider = this.getDataProvider(target);
     if (dataProvider && dataProvider.type) {
       const data = await this.getDataByProviderAsync(dataProvider);
-      console.log("refreshData", data);
       this.setData(target, data);
       this.refresh(target);
     }
   }
 
-  private startRefreshInterval(target: any, interval: number) {
+  // DataProvider의 interval에 따라 주기적으로 데이터를 업데이트합니다.
+  private static startRefreshInterval(target: any, interval: number) {
     this.stopRefreshInterval(target);
     if (interval > 0) {
       const options = this.getOptions(target);
-      options.refreshIntervalId = setInterval(() => {
+      options.refreshId = setInterval(() => {
         this.refreshData(target);
       }, interval);
     }
   }
 
-  private stopRefreshInterval(target: any) {
+  // DataProvider의 interval을 중지합니다.
+  private static stopRefreshInterval(target: any) {
     const options = this.getOptions(target);
-    const refreshIntervalId = options.refreshIntervalId;
-    if (refreshIntervalId) {
-      clearInterval(refreshIntervalId);
-      options.refreshIntervalId = undefined;
+    const refreshId = options.refreshId;
+    if (refreshId) {
+      clearInterval(refreshId);
+      options.refreshId = undefined;
     }
   }
 
-  // ## public methods...
-  binding(p: { target: any; props: any; data?: any; dataProvider?: any }) {
-    this.setProps(p.target, p.props);
-    if (p.data) {
-      this.setData(p.target, p.data);
-    }
+  /** ==================== DataProvider Main Methods End ==================== **/
 
-    if (p.dataProvider) {
-      this.updateDataProvider({
-        target: p.target,
-        dataProvider: p.dataProvider,
-      });
-    }
 
-    this.refresh(p.target);
+
+  /** ==================== Public Methods Start ==================== **/
+
+  /**
+   * 타겟 오브젝트에 속성(props)를 설정하고, 데이터(data)를 jsonata 문법으로 바인딩합니다, 데이터 제공자(dataProvider)를 설정시, 데이터를 주기적으로 업데이트합니다.
+   * @param p - 바인딩 설정
+   * @param p.target - 타겟 오브젝트(HTMLElement, Object, Class, ...)
+   * @param p.props - 타겟 오브젝트에 설정할 속성
+   * @param p.data - 속성값에 바인딩할 데이터
+   * @param p.dataProvider - 데이터 제공자 설정
+   * @example
+   * ```ts
+   * const target = document.querySelector("target");
+   * Bind.binding({ 
+   *    target: target,
+   *    props: { style: "color: {{color}}" },
+   *    data: { color: "red" },
+   *    dataProvider: { type: "webapi", url: "https://api.example.com/data" }
+   * });
+   * ```
+   */
+  public static binding(p: { target: any, props: any, data?: any, dataProvider?: DataProvider }) {
+    const { target, props, data, dataProvider } = p;
+    this.setProps(target, props);
+    if (data) {
+      this.setData(target, data);
+    }
+    if (dataProvider) {
+      this.updateDataProvider({ target, dataProvider });
+    }
+    this.refresh(target);
   }
 
-  updateProps(p: { target: any; props: any }, withRefresh: boolean = true) {
-    this.setProps(p.target, p.props);
-
-    if (withRefresh) {
-      this.refresh(p.target);
+  /**
+   * 바인드된 타겟의 속성을 업데이트하고, 필요시 타겟을 갱신합니다.
+   * @param p - 설정할 파라미터
+   * @param refresh - 업데이트 속성을 즉시 반영할지 설정합니다. 기본값은 true입니다.
+   */
+  public static updateProps(p: { target: any, props: any }, refresh: boolean = true) {
+    const { target, props } = p;
+    this.setProps(target, props);
+    if (refresh) {
+      this.refresh(target);
     }
   }
 
-  updateData(p: { target: any; data: any }, withRefresh: boolean = true) {
-    this.setData(p.target, p.data);
-
-    if (withRefresh) {
-      this.refresh(p.target);
+  /**
+   * 바인드된 타겟의 데이터를 업데이트하고, 필요시 타겟을 갱신합니다.
+   * @param p - 설정할 파라미터
+   * @param refresh - 업데이트 데이터를 즉시 반영할지 설정합니다. 기본값은 true입니다.
+   */
+  public static updateData(p: { target: any, data: any }, refresh: boolean = true) {
+    const { target, data } = p;
+    this.setData(target, data);
+    if (refresh) {
+      this.refresh(target);
     }
   }
 
-  updateDataProvider(p: { target: any; dataProvider: any }) {
-    this.setDataProvider(p.target, p.dataProvider);
-    this.refreshData(p.target);
-
-    const refreshInterval = p.dataProvider.refreshInterval || 0;
-    this.startRefreshInterval(p.target, refreshInterval);
+  /**
+   * 바인딩된 타겟의 데이터 제공자를 설정합니다.
+   * @param p - 설정할 파라미터
+   * @param p.target - 바인드된 타겟 오브젝트
+   * @param p.dataProvider - 설정할 데이터 제공자
+   */
+  public static updateDataProvider(p: { target: any, dataProvider: DataProvider }) {
+    const { target, dataProvider } = p;
+    this.setDataProvider(target, dataProvider);
+    this.refreshData(target);
+    const interval = dataProvider.interval || 0;
+    this.startRefreshInterval(target, interval);
   }
 
-  refresh(target: any) {
+  /**
+   * 바인드된 타겟의 속성에 데이터를 갱신합니다.
+   * @param target - 바인드된 타겟 오브젝트
+   */
+  public static refresh(target: any) {
     const props = this.getProps(target);
     const data = this.getData(target);
     this.applyProps(target, props, data);
   }
-}
 
-export const bind = new Bind();
+  /** ==================== Public Methods Start ==================== **/
+
+}
