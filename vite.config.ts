2678a343cf83828a39c4d2f64f817bdc09bfbcc9
iujunhyung <junehyung@iyulab.com>
2025-11-13 19:30:14 +0900

    update

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,16 @@ import { defineConfig } from 'vite'
 import { resolve } from 'path'
 import dts from 'vite-plugin-dts'
 
-// https://vite.dev/config/
 export default defineConfig({
   build: {
+    outDir: 'dist',
+    emptyOutDir: true,
     lib: {
-      name: 'TemplateBinding',
       entry: resolve(__dirname, 'src/index.ts'),
       formats: ['es', 'cjs'],
-      fileName: (format) => format === 'es' ? 'index.js' : `index.${format}.js`,
+      fileName: (format) => {
+        return format === 'es' ? `index.js` : `index.${format}.js`
+      }
     },
     rollupOptions: {
       external: [
@@ -17,15 +19,11 @@ export default defineConfig({
         /^jsonata.*/
       ]
     },
-    outDir: 'dist',
-    emptyOutDir: true
   },
   plugins: [
     dts({
-      outDir: 'dist',
+      include: ['src/**/*'],
       rollupTypes: true,
-      insertTypesEntry: true,
-      tsconfigPath: resolve(__dirname, 'tsconfig.json')
-    }) as any
+    })
   ]
 })
